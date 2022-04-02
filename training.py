b37d317867054dece8a633ae427523dfadf6729f
dufaultc <dufault77@gmail.com>
2022-04-02 19:42:53 -0400

    print

--- training.py
+++ training.py
@@ -33,6 +33,7 @@ def run_training(parsed_details, train_request):
         )
 
     model =  torchvision.models.resnet101(pretrained=True)
+    print(model)
     model.classifier[1] = torch.nn.Linear(in_features=model.classifier[1].in_features,out_features=1)
     agh = InfractionDetectionModel(train_set, val_set, model, col_fn) 
     trainer = pl.Trainer(max_epochs=5)
