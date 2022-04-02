58bd5f52750919c3bc70be8830fd30eb2b1b842e
dufaultc <dufault77@gmail.com>
2022-04-02 19:04:29 -0400

    one more time one more time

--- training.py
+++ training.py
@@ -35,7 +35,7 @@ def run_training(parsed_details, train_request):
     model =  torchvision.models.mobilenet_v2(pretrained=True)
     model.classifier[1] = torch.nn.Linear(in_features=model.classifier[1].in_features,out_features=1)
     agh = InfractionDetectionModel(train_set, val_set, model, col_fn) 
-    trainer = pl.Trainer(max_epochs=10)
+    trainer = pl.Trainer(max_epochs=10, accumulate_grad_batches=4)
     print('begin training')
     trainer.fit(agh)
     print(trainer)
